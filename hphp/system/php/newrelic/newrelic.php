fb025a982a9cedd4bfb48ff205f830b6f7329c96
Christian Stocker <me@chregu.tv>
2014-02-25 08:10:13 +0000

    enable functions written in php for the newrelic profiler

--- hphp/system/php/newrelic/newrelic.php
+++ hphp/system/php/newrelic/newrelic.php
@@ -1,11 +1,5 @@
 <?php
 
-function newrelic_profiling_enable(int $level) {
-  xhprof_enable(256,array(0 => $level));
+function newrelic_hotprofiling_enabled_intern() {
+    return true;
 }
-
-
-function newrelic_profiling_disable() {
-    xhprof_disable();
-}
-
