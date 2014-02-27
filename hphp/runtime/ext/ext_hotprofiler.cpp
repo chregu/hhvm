f0250eab9f9e73a44c77ca3facd2431ef58445a8
Christian Stocker <me@chregu.tv>
2014-02-27 21:11:14 +0100

    set nr_id to 0

--- hphp/runtime/ext/ext_hotprofiler.cpp
+++ hphp/runtime/ext/ext_hotprofiler.cpp
@@ -794,6 +794,7 @@ public:
   virtual void endFrameEx() {
     if ( m_stack->nr_id != 0) {
 		newrelic_segment_end(NEWRELIC_AUTOSCOPE, m_stack->nr_id);
+		m_stack->nr_id = 0;
 	}
   }
   
@@ -801,7 +802,6 @@ public:
     while (m_stack) {
       endFrame(nullptr, true);
     }
-    newrelic_transaction_end(NEWRELIC_AUTOSCOPE);
   }
 
 private:
