33f6353001e966ceec0f93552c400f10121ca015
Christian Stocker <me@chregu.tv>
2014-03-29 11:10:40 +0100

    fix compile error on master/3.0 branch

--- hphp/runtime/ext/ext_hotprofiler.cpp
+++ hphp/runtime/ext/ext_hotprofiler.cpp
@@ -777,9 +777,9 @@ public:
 	  if (! f_extension_loaded(s__NEWRELIC)) {
 		  GlobalVariables *g = get_global_variables();
 		  newrelic_transaction_begin();
-		  String request_url = g->get(s__SERVER)[s__REQUEST_URI].toString();
+		  String request_url = g->get(s__SERVER).toArray()[s__REQUEST_URI].toString();
 		  newrelic_transaction_set_request_url(NEWRELIC_AUTOSCOPE, request_url.c_str());
-		  String script_name = g->get(s__SERVER)[s__SCRIPT_NAME].toString();
+		  String script_name = g->get(s__SERVER).toArray()[s__SCRIPT_NAME].toString();
 		  newrelic_transaction_set_name(NEWRELIC_AUTOSCOPE, script_name.c_str());
 	  }
   }
